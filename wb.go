cecdab34f1ed0f30cb40428e1773ba6fe87ea30b
ulrich.schreiner@gmail.com <usc@develop.(none)>
2012-11-09 20:09:12 +0100

    nix

--- wb.go
+++ wb.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
-func main () {
-  fmt.Println ("Hallo")
+var (
+	concurrent = flag.Int("c", 1, "Number of multiple requests to make")
+	requests   = flag.Int("n", 1, "Number of requests to perform")
+)
+
+func main() {
+	fmt.Println("Hallo Welt")
 }
