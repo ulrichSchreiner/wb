9a4bc2f704d7882710ac51a3c3849642c833f0a7
Ulrich Schreiner <usc@hp-pavilion-usc.(none)>
2012-11-10 21:10:16 +0100

    - Proxy aus der Umgebung lesen - Nur noch einen einzigen HTTP-Client, der ist parallel-safe - Ausgabedaten auslesen - SSL Zertifikate akzeptieren, auch wenn diese unsicher sind

--- wb.go
+++ wb.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
-	"fmt"
 )
 
 var (
 	concurrent = flag.Int("c", 1, "Number of multiple requests to make")
 	requests   = flag.Int("n", 1, "Number of requests to perform")
-	verbosity  = flag.Bool("v", false, "Show info while running")
+	verbosity  = flag.Int("v", 0, "Show info while running")
+)
+
+const (
+	VERBOSE_NOTHING = 0
+	VERBOSE_MIN     = 1
+	VERBOSE_MAX     = 2
 )
 
 type URLRequest struct {
@@ -21,112 +29,141 @@ type URLResponse struct {
 	Time          time.Duration
 	Status        int
 	ContentLength int64
-	Error	error
+	Body          string
+	Error         error
 }
 
 type URLFetcher struct {
-	Client http.Client
+	Client *http.Client
 }
 
 type Statistics struct {
-	TimeMinimum	time.Duration
-	TimeMaximum	time.Duration
-	NumCalls	int
-	TimeAggregate	int64
+	TimeMinimum   time.Duration
+	TimeMaximum   time.Duration
+	NumCalls      int
+	TimeAggregate int64
 
-	StatusCodes	map[int]int
+	StatusCodes map[int]int
 }
 
 func (c *URLFetcher) loadURL(rq *URLRequest) (*URLResponse, error) {
 	response := new(URLResponse)
 	t0 := time.Now()
 	rsp, err := c.Client.Get(rq.URL)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
+	body, err := ioutil.ReadAll(rsp.Body)
 	response.Time = time.Since(t0)
 	if err != nil {
 		return nil, err
 	}
+	response.Body = string(body)
 	response.Status = rsp.StatusCode
 	response.ContentLength = rsp.ContentLength
-
+	if rsp.ContentLength == -1 {
+		response.ContentLength = int64(len(body))
+	}
 	return response, nil
 }
 
-func makeFetcher (inc chan *URLRequest, out chan *URLResponse) {
-	f := new (URLFetcher)
-	go func () {
-		for rq := range(inc) {
+func makeFetcher(f *URLFetcher, inc chan *URLRequest, out chan *URLResponse) {
+	go func() {
+		for rq := range inc {
 			rsp, err := f.loadURL(rq)
 			if err != nil {
-				rsp = new (URLResponse)
+				rsp = new(URLResponse)
 				rsp.Error = err
 			}
 			out <- rsp
 		}
-	} ()	
+	}()
 }
 
-func (s *Statistics) Collector (idx int, rsp *URLResponse) {
+func (s *Statistics) Collector(idx int, rsp *URLResponse) {
 	nsMin := s.TimeMinimum.Nanoseconds()
 	nsMax := s.TimeMaximum.Nanoseconds()
 	ns1 := rsp.Time.Nanoseconds()
-	s.TimeAggregate = s.TimeAggregate + ns1	
+	s.TimeAggregate = s.TimeAggregate + ns1
 	s.NumCalls++
-	s.StatusCodes[rsp.Status] ++
+	s.StatusCodes[rsp.Status]++
 
 	switch {
-		case s.NumCalls == 1:
-			s.TimeMinimum = rsp.Time
-			s.TimeMaximum = rsp.Time
-		case ns1 < nsMin:
-			s.TimeMinimum = rsp.Time
-		case ns1 > nsMax:
-			s.TimeMaximum = rsp.Time
+	case s.NumCalls == 1:
+		s.TimeMinimum = rsp.Time
+		s.TimeMaximum = rsp.Time
+	case ns1 < nsMin:
+		s.TimeMinimum = rsp.Time
+	case ns1 > nsMax:
+		s.TimeMaximum = rsp.Time
 	}
-	if *verbosity {
+	if *verbosity == VERBOSE_MAX {
+		if rsp.Error != nil {
+			fmt.Printf("%#v\n", rsp.Error)
+		}
 		mi := s.TimeMinimum / time.Millisecond
 		mx := s.TimeMaximum / time.Millisecond
 		av := (s.TimeAggregate / int64(s.NumCalls)) / time.Millisecond.Nanoseconds()
 		call := rsp.Time / time.Millisecond
-		fmt.Printf ("%6d: Call: %6dms|Min: %6dms|Avg: %6dms|Max: %6dms\n",(idx+1),call, mi,av,mx)
+		fmt.Printf("%6d: Call: %6dms|Min: %6dms|Avg: %6dms|Max: %6dms|Stat:%3d|Len:%6d\n", (idx + 1), call, mi, av, mx, rsp.Status, rsp.ContentLength)
+	}
+	if *verbosity == VERBOSE_MIN {
+		if s.NumCalls%100 == 0 {
+			s.Dump()
+		}
 	}
 }
 
-func (s *Statistics) Dump () {
+func (s *Statistics) Dump() {
 	mi := s.TimeMinimum / time.Millisecond
 	mx := s.TimeMaximum / time.Millisecond
 	av := (s.TimeAggregate / int64(s.NumCalls)) / time.Millisecond.Nanoseconds()
-	fmt.Printf ("Min: %6dms\tAvg: %6dms\tMax: %6dms\n",mi,av,mx)
+	fmt.Printf("Calls: %6d\tMin: %6dms\tAvg: %6dms\tMax: %6dms\n", s.NumCalls, mi, av, mx)
 }
 
-func NewStatistics () *Statistics {
-	s := new (Statistics)
+func NewStatistics() *Statistics {
+	s := new(Statistics)
 	s.StatusCodes = make(map[int]int)
 	return s
 }
 
-func responseDumper (rsp *URLResponse) {
+func responseDumper(rsp *URLResponse) {
 	//log.Printf ("%#v\n", rsp)
+}
+
+func NewFetcher() *URLFetcher {
+	fetcher := new(URLFetcher)
+	tr := &http.Transport{
+		Proxy:           http.ProxyFromEnvironment,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	fetcher.Client = &http.Client{Transport: tr}
+	return fetcher
 }
 
 func main() {
 	flag.Parse()
 	stats := NewStatistics()
-	var requestList = make (chan *URLRequest, *requests)
-	var responseList = make (chan *URLResponse, *requests)
+	requestList := make(chan *URLRequest, *requests)
+	responseList := make(chan *URLResponse, *requests)
+	fetcher := NewFetcher()
 
-	for c:=0; c<*concurrent; c++ {
-		makeFetcher (requestList, responseList)
+	for c := 0; c < *concurrent; c++ {
+		makeFetcher(fetcher, requestList, responseList)
 	}
 	u := flag.Arg(0)
-	for i:=0; i<*requests; i++ {
-		r := URLRequest {
-			URL:u}
+	for i := 0; i < *requests; i++ {
+		r := URLRequest{
+			URL: u}
 		requestList <- &r
-      	}
-	for i:=0; i<*requests; i++ {
-		rsp := <-responseList
-		stats.Collector (i,rsp)
-		responseDumper (rsp)
 	}
-	stats.Dump ()
+	for i := 0; i < *requests; i++ {
+		rsp := <-responseList
+		stats.Collector(i, rsp)
+		responseDumper(rsp)
+	}
+	fmt.Println("Result:")
+	fmt.Printf("URL: %s\n", u)
+	stats.Dump()
 }
