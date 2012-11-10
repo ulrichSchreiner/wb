34e7a47f2bb16cd70790cebcc3b1348136e6f5ac
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2012-11-10 15:47:26 +0100

    grundfunktionalität eingebaut

--- wb.go
+++ wb.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"net/http"
 	"time"
-	"log"
+	"fmt"
 )
 
 var (
 	concurrent = flag.Int("c", 1, "Number of multiple requests to make")
 	requests   = flag.Int("n", 1, "Number of requests to perform")
+	verbosity  = flag.Bool("v", false, "Show info while running")
 )
 
 type URLRequest struct {
@@ -26,6 +26,15 @@ type URLResponse struct {
 
 type URLFetcher struct {
 	Client http.Client
+}
+
+type Statistics struct {
+	TimeMinimum	time.Duration
+	TimeMaximum	time.Duration
+	NumCalls	int
+	TimeAggregate	int64
+
+	StatusCodes	map[int]int
 }
 
 func (c *URLFetcher) loadURL(rq *URLRequest) (*URLResponse, error) {
@@ -56,20 +65,68 @@ func makeFetcher (inc chan *URLRequest, out chan *URLResponse) {
 	} ()	
 }
 
-func responseDumper (rsp *URLResponse) {
-	log.Printf ("%#v\n", rsp)
+func (s *Statistics) Collector (idx int, rsp *URLResponse) {
+	nsMin := s.TimeMinimum.Nanoseconds()
+	nsMax := s.TimeMaximum.Nanoseconds()
+	ns1 := rsp.Time.Nanoseconds()
+	s.TimeAggregate = s.TimeAggregate + ns1	
+	s.NumCalls++
+	s.StatusCodes[rsp.Status] ++
+
+	switch {
+		case s.NumCalls == 1:
+			s.TimeMinimum = rsp.Time
+			s.TimeMaximum = rsp.Time
+		case ns1 < nsMin:
+			s.TimeMinimum = rsp.Time
+		case ns1 > nsMax:
+			s.TimeMaximum = rsp.Time
+	}
+	if *verbosity {
+		mi := s.TimeMinimum / time.Millisecond
+		mx := s.TimeMaximum / time.Millisecond
+		av := (s.TimeAggregate / int64(s.NumCalls)) / time.Millisecond.Nanoseconds()
+		call := rsp.Time / time.Millisecond
+		fmt.Printf ("%6d: Call: %6dms|Min: %6dms|Avg: %6dms|Max: %6dms\n",(idx+1),call, mi,av,mx)
+	}
 }
 
-var requestList = make (chan *URLRequest, 100)
-var responseList = make (chan *URLResponse, 100)
+func (s *Statistics) Dump () {
+	mi := s.TimeMinimum / time.Millisecond
+	mx := s.TimeMaximum / time.Millisecond
+	av := (s.TimeAggregate / int64(s.NumCalls)) / time.Millisecond.Nanoseconds()
+	fmt.Printf ("Min: %6dms\tAvg: %6dms\tMax: %6dms\n",mi,av,mx)
+}
+
+func NewStatistics () *Statistics {
+	s := new (Statistics)
+	s.StatusCodes = make(map[int]int)
+	return s
+}
+
+func responseDumper (rsp *URLResponse) {
+	//log.Printf ("%#v\n", rsp)
+}
 
 func main() {
-	makeFetcher (requestList, responseList)
-	r := URLRequest {
-		URL:"http://www.google.de/"}
-	requestList <- &r
-	for rsp := range (responseList) {
+	flag.Parse()
+	stats := NewStatistics()
+	var requestList = make (chan *URLRequest, *requests)
+	var responseList = make (chan *URLResponse, *requests)
+
+	for c:=0; c<*concurrent; c++ {
+		makeFetcher (requestList, responseList)
+	}
+	u := flag.Arg(0)
+	for i:=0; i<*requests; i++ {
+		r := URLRequest {
+			URL:u}
+		requestList <- &r
+      	}
+	for i:=0; i<*requests; i++ {
+		rsp := <-responseList
+		stats.Collector (i,rsp)
 		responseDumper (rsp)
 	}
-	fmt.Println("Hallo")
+	stats.Dump ()
 }
